23050b8ad039aaecb46e3baaf35506901f01db38
Ayushmangiri <ayushmangiri@gmail.com>
2026-01-19 18:21:46 +0530

    Added the demo project

---
+++ src/main/java/com/example/keystore_demo/KeystoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.keystore_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeystoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeystoreDemoApplication.class, args);
+	}
+
+}
